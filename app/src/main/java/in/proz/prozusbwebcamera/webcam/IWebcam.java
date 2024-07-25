9b9c3531d63dac10fa68625c427302e4ee589f16
ADMIN\DELL LATITUDE 5400 <keerthiga@pro-z.in>
2024-07-25 11:31:31 +0530

    Basic Structure

--- app/src/main/java/in/proz/prozusbwebcamera/webcam/IWebcam.java
+++
@@ -1,9 +0,0 @@
-package in.proz.prozusbwebcamera.webcam;
-
-import android.graphics.Bitmap;
-
-public interface IWebcam {
-    public Bitmap getFrame();
-    public void stop();
-    public boolean isAttached();
-}
